1ef75376f6b6db119ab9c7588719d4f17459364c
Vincent Leong <lytv0511@icloud.com>
2024-10-29 12:18:52 -0400

    Initial Commit

---
+++ NutrivanceTests/NutrivanceTests.swift
@@ -0,0 +1,17 @@
+//
+//  NutrivanceTests.swift
+//  NutrivanceTests
+//
+//  Created by Vincent Leong on 10/29/24.
+//
+
+import Testing
+@testable import Nutrivance
+
+struct NutrivanceTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
